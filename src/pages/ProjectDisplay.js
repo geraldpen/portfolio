dcd3217d1e1132fb1a0109327b510477fb4444ec
geraldpen <102095892+geraldpen@users.noreply.github.com>
2023-04-26 08:20:10 +0100

    the project page is comented

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { useParams} from 'react-router-dom';
-import { ProjectList } from '../helper/ProjectList';
-import {FaGithub} from 'react-icons/fa';
-import "../styles/ProjectDisplay.css";
+// import React from 'react';
+// import { useParams} from 'react-router-dom';
+// import { ProjectList } from '../helper/ProjectList';
+// import {FaGithub} from 'react-icons/fa';
+// import "../styles/ProjectDisplay.css";
 
-function ProjectDisplay() {
-    const {id} = useParams();
-    const project = ProjectList[id]
-  return (
-  <div className="project"> 
-  <h1> {project.name}</h1>
-  <img src={project.image} alt=""/>
-  <FaGithub/>
-  </div>
+// function ProjectDisplay() {
+//     const {id} = useParams();
+//     const project = ProjectList[id]
+//   return (
+//   <div className="project"> 
+//   <h1> {project.name}</h1>
+//   <img src={project.image} alt=""/>
+//   <FaGithub/>
+//   </div>
   
-  )
+//   )
   
-}
+// }
 
 export default ProjectDisplay
